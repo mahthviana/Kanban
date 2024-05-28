a9a04745d5d5ccbd74fd523e3b9128d385c91f0b
mahthviana <94473682+mahthviana@users.noreply.github.com>
2024-05-28 01:46:59 -0300

    Add files via upload
    (tentativa de ) adição de funcionalidade arrasta e solta.

--- toDo.js
+++ toDo.js
@@ -1,25 +1,106 @@
-document.addEventListener ('DOMContentLoaded', ()=> {
-    const inputNewTask = document.getElementById('new-task');
-    const newTaskBtn = document.querySelector('.adicionar');
-    const TaskListToDo = document.querySelector('.toDo');
+// input para criação de nova tarefa
+const newTaskInput = document.getElementById('new-task');
+const newTaskBtn = document.querySelector('.adicionar');
 
-    const addTask = () => {
-        const taskText = inputNewTask.value.trim();
-        if (taskText != "") {
-            const li = document.createElement('li');
+// colunas de tarefas
+const toDo = document.querySelector('.toDo');
+const inProgress = document.querySelector('.inProgress');
+const concluded = document.querySelector('.concluded');
 
-            li.innerHTML = `<span>${taskText}</span>`;
+//  cria nova tarefa
+const addTask = () => {
+    const inputValue = newTaskInput.value.trim();
+    if (newTaskInput !== "") {
+        
+        const p = document.createElement('p');
+        p.innerHTML =  `${inputValue}`;
 
-            TaskListToDo.appendChild(li)
+        p.setAttribute('draggable', 'true');
+        
+        p.classList.add('task');
 
-            inputNewTask.value = ""
-        }
+        toDo.appendChild(p);
+        newTaskInput.value = "";
+
+        p.addEventListener('dragstart', dragStart);
+        
+
     }
-    newTaskBtn.addEventListener('click', addTask)
+}
 
-    inputNewTask.addEventListener('keypress', (event) => {
-        if(event.key === 'Enter') {
-            addTask()
-        }
-    })
+const dragStart = (event) => {
+    event.dataTransfer.setData('text/plain', event.target.id);
+}
+
+const dragOver = (event) => {
+    event.preventDefault();
+}
+
+const dragEnter = (event) => {
+    event.preventDefault();
+}
+
+const dragLeave = (event) => {
+    event.preventDefault();
+}
+
+// zona para dropar o elemento
+const drop = (event) => {
+    event.preventDefault()
+    console.log(event)
+    const data = event.dataTransfer.getData('text/plain');
+    
+    const draggedElement = document.getElementById(data);
+    console.log(draggedElement)
+
+    const targetDiv = event.target.closest('.containerAddTask').querySelector('.task-container')
+    console.log(targetDiv)
+
+    draggedElement.parentNode.removeChild(draggedElement);
+
+
+
+    if (targetDiv.classList.contains('inProgress')) {
+        draggedElement.classList.add('inProgress');
+    } else if (targetDiv.classList.contains('concluded')) {
+        draggedElement.classList.add('concluded');
+    }
+}
+
+// adição de manipuladores de evento para as divs
+inProgress.addEventListener('dragover', dragOver);
+inProgress.addEventListener('dragenter', dragEnter);
+inProgress.addEventListener('dragleave', dragLeave);
+inProgress.addEventListener('drop', drop);
+
+concluded.addEventListener('dragover', dragOver);
+concluded.addEventListener('dragenter', dragEnter);
+concluded.addEventListener('dragleave', dragLeave);
+concluded.addEventListener('drop', drop);
+
+// adciona a tarefa na coluna toDo
+newTaskBtn.addEventListener('click', addTask)
+newTaskInput.addEventListener('keypress', (event) => {
+    if(event.key === 'Enter') {
+        addTask()
+    }
+})
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const tasks = document.querySelectorAll('.task');
+
+    // Adicione manipuladores de eventos de arrastar para cada tarefa
+    tasks.forEach(task => {
+        task.addEventListener('dragstart', dragStart);
+    });
+
+    // Adicione manipuladores de eventos de arrastar nas divs de destino
+    const dropzones = document.querySelectorAll('.inProgress, .concluded');
+    dropzones.forEach(dropzone => {
+        dropzone.addEventListener('dragover', dragOver);
+        dropzone.addEventListener('dragenter', dragEnter);
+        dropzone.addEventListener('dragleave', dragLeave);
+        dropzone.addEventListener('drop', drop);
+    });
 });
